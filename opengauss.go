f32a1a87ac9d229ed6a526e9251b4a09a0f61b61
erlzhang <zhangshiyu1992@hotmail.com>
2023-03-22 14:21:49 +0800

    debug

--- opengauss.go
+++ opengauss.go
@@ -127,7 +127,7 @@ func (dialector Dialector) ClauseBuilders() map[string]clause.ClauseBuilder {
 
 			builder.WriteString("ON DUPLICATE KEY UPDATE ")
 			fmt.Println("========DoUpdates: onConflict.DoUpdates")
-			if len(onConflict.DoUpdates) == 0 {
+			if len(onConflict.DoUpdates) != 0 {
 				if s := builder.(*gorm.Statement).Schema; s != nil {
 					var column clause.Column
 					onConflict.DoNothing = false
